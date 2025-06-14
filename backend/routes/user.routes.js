99e336c13734915719772341291a67bfbc6aafbf
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-06-14 23:48:23 +0530

    user login done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -11,4 +11,11 @@ router.post("/register", [
     userController.registerUser
 )
 
+router.post("/login", [
+    body("email").isEmail().withMessage("invalid email"),
+    body("password").isLength({min: 6}).withMessage("Password") 
+    ],
+    userController.loginUser
+)
+
 module.exports= router
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -4,6 +4,12 @@ module.exports.createUser= async ({firstname, lastname, email, password})=>{
     if (!firstname || !email || !password){
         throw new Error("all fields are required")
     }
+
+    const existingUser= await userModel.findOne({email});
+    if (existingUser) {
+        throw new Error("User with this email already exists.");
+    }
+
     const user= await userModel.create({
         fullname:{
             firstname, lastname

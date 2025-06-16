bffbd0ce3596ea4ceea0e735e5a374b779b864db
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-06-16 13:25:02 +0530

    logout completed

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,8 @@ const express= require("express")
 const router=express.Router()
 const {body}= require("express-validator")
 const userController= require("../controllers/user.controller")
+const authMiddleware= require("../middlewares/auth.middleware")
+const blackListTokenModel=require("../models/blacklistToken.model")
 
 router.post("/register", [
     body("email").isEmail().withMessage("invalid email"),
@@ -17,5 +19,9 @@ router.post("/login", [
     ],
     userController.loginUser
 )
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+router.get("/logout", authMiddleware.authUser, userController.logoutUser)
 
 module.exports= router
